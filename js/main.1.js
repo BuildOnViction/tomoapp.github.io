6ac55c2baa833e6dc9937e4018ba2bda3756adfe
TungHT <hoangtung.utc@gmail.com>
2017-09-08 16:12:49 +0700

    update

--- js/main.1.js
+++ js/main.1.js
@@ -118,13 +118,13 @@ $(document).ready(function () {
   //------------------------------------//
   //Wow Animation//
   //------------------------------------//
-  wow = new WOW(
-    {
-      boxClass: 'wow',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      offset: 0,          // distance to the element when triggering the animation (default is 0)
-      mobile: false        // trigger animations on mobile devices (true is default)
-    }
-  );
-  wow.init();
+  // wow = new WOW(
+  //   {
+  //     boxClass: 'wow',      // animated element css class (default is wow)
+  //     animateClass: 'animated', // animation css class (default is animated)
+  //     offset: 0,          // distance to the element when triggering the animation (default is 0)
+  //     mobile: false        // trigger animations on mobile devices (true is default)
+  //   }
+  // );
+  // wow.init();
 });
